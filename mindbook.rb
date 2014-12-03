984d5a9637eaf5ed813b498ae54d1adebc158f3f
alu0100696677 <alu0100696677@ull.edu.es>
2014-12-03 10:29:01 +0000

    se implementa datamapper mindbook.rb

--- mindbook.rb
+++ mindbook.rb
@@ -5,6 +5,31 @@ require 'data_mapper'
 #set :port, 3000
 #set :environment, :production
 
+enable :sessions
+set :session_secret, '*&(^#234a)'
+
+configure :development do
+	DataMapper.setup( :default, ENV['DATABASE_URL'] || 
+                            "sqlite3://#{Dir.pwd}/mindbook.db" )
+end
+
+configure :production do   #heroku
+	DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
+configure :test do
+	DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/test.db")
+end
+
+DataMapper::Logger.new($stdout, :debug)
+DataMapper::Model.raise_on_save_failure = true 
+
+require_relative 'model'
+
+DataMapper.finalize
+
+DataMapper.auto_upgrade!
+
 get '/' do 
         erb :index
 end
---
+++ mindbook.rb~
@@ -0,0 +1,64 @@
+require 'sinatra' 
+require 'sinatra/reloader' if development?
+require 'sinatra/flash'
+require 'data_mapper'
+#set :port, 3000
+#set :environment, :production
+
+get '/' do 
+        erb :index
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
