9f1d99695bd2d3aa182093e57758de95d9ce7700
alu0100696677 <alu0100696677@ull.edu.es>
2014-12-12 14:08:31 +0000

    1º test selenium

--- test/test.rb
+++ test/test.rb
@@ -14,3 +14,20 @@ include Rack::Test::Methods
 def app
 	Sinatra::Application
 end
+
+describe "test selenium pagina registro" do
+
+	before :all do
+		@browser = Selenium::WebDriver.for :firefox
+		@url = 'https://mindbooks.herokuapp.com/'
+		if (ARGV[0].to_s == "localhost")
+		@url = 'localhost:4567/'
+	end
+		@browser.get(@url)
+	end
+
+	it "se abre pagina de Inicio?" do
+		assert_equal("https://mindbooks.herokuapp.com/",@browser.current_url)
+		@browser.quit
+	end
+end
