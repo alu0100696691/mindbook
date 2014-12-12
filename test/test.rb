2e8c783108bc8c9d56504a5412d700510e33c8db
alu0100724622 <alu0100724622@ull.edu.es>
2014-12-12 14:32:27 +0000

    test selenium envio de email

--- test/test.rb
+++ test/test.rb
@@ -15,7 +15,7 @@ def app
 	Sinatra::Application
 end
 
-describe "test selenium pagina registro" do
+describe "test selenium pagina inicio" do
 
 	before :all do
 		@browser = Selenium::WebDriver.for :firefox
@@ -23,11 +23,30 @@ describe "test selenium pagina registro" do
 		if (ARGV[0].to_s == "localhost")
 		@url = 'localhost:4567/'
 	end
+		@wait = Selenium::WebDriver::Wait.new(:timeout => 5)
 		@browser.get(@url)
 	end
 
-	it "se abre pagina de Inicio?" do
-		assert_equal("https://mindbooks.herokuapp.com/",@browser.current_url)
+	after :all do
 		@browser.quit
 	end
+
+	it "se abre pagina de Inicio?" do
+		assert_equal("https://mindbooks.herokuapp.com/",@browser.current_url)		
+	end
+
+	it "funciona enviar email?" do		
+		begin
+		element = @browser.find_element(:link,"Contacto")
+		ensure
+		element.click
+		@browser.find_element(:id,"name2").send_keys("juan")
+		@browser.find_element(:id,"email2").send_keys("juan@juan")
+		@browser.find_element(:id,"message2").send_keys("mensaje de juan")
+		@browser.find_element(:id,"footer-form").submit()
+		assert_equal("https://mindbooks.herokuapp.com/?nombre=juan&email=juan%40juan&mensaje=mensaje+de+juan",@browser.current_url)
+		end
+	end
+
+	
 end
